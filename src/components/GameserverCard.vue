0fddf9c63c54ecb33702c050e5f52ff83d066a3b
Chaton <ethan@ehsawyer.uk>
2024-05-22 21:18:31 +0100

    Updated Gameservers formatting

--- src/components/GameserverCard.vue
+++ src/components/GameserverCard.vue
@@ -11,20 +11,21 @@
       <div class="card-body d-grid">
         <h3 class="card-title">{{ gameserver.name }}</h3>
         <div id="badges" class="mb-4 mt-1">
-          <span v-if="gameserver.status === 'running'" class="badge badge-pill bg-success">{{ gameserver.status }}</span>
-          <span v-else-if="gameserver.status === 'starting'" class="badge badge-pill text-dark bg-warning">{{ gameserver.status }}</span>
-          <span v-else-if="gameserver.status === 'offline'" class="badge badge-pill bg-danger">{{ gameserver.status }}</span>
-          <span class="px-1"></span>
-          <span v-if="gameserver.password === 'whitelist'" class="badge badge-pill bg-success">whitelist enabled</span>
-          <span v-else-if="gameserver.password" class="badge badge-pill bg-success">password enabled</span>
-          <span v-else="!gameserver.password" class="badge badge-pill bg-danger">no security</span>
+          <span v-if="gameserver.status === 'running'" class="badge badge-pill bg-success mx-1">{{ gameserver.status }}</span>
+          <span v-else-if="gameserver.status === 'starting'" class="badge badge-pill text-dark bg-warning mx-1">{{ gameserver.status }}</span>
+          <span v-else-if="gameserver.status === 'offline'" class="badge badge-pill bg-danger mx-1">{{ gameserver.status }}</span>
+          <span v-if="gameserver.password === 'whitelist'" class="badge badge-pill bg-success mx-1">whitelist enabled</span>
+          <span v-else-if="gameserver.password" class="badge badge-pill bg-success mx-1">password enabled</span>
+          <span v-else="!gameserver.password" class="badge badge-pill bg-danger mx-1">no security</span>
+          <span v-if="gameserver.version.primary" class="badge badge-pill bg-info text-dark mx-1">{{ gameserver.version.primary }}</span>
+          <span v-if="gameserver.version.secondary" class="badge badge-pill bg-info text-dark mx-1">{{ gameserver.version.secondary }}</span>
         </div>
         <p class="card-text">{{ gameserver.description }}</p>
         <!-- <a :href="gameserver.link" class="btn btn-primary mb-2">Random URL</a> -->
-        <h5 class="mt-2">Connection address</h5>
-        <ClickCopy :content="gameserver.address" />
-        <h5 class="mt-2" v-if="gameserver.password != 'whitelist'">Password</h5>
-        <ClickCopy v-if="gameserver.password != 'whitelist'" :content="gameserver.password" />
+        <h5 v-if="gameserver.address" class="mt-2">Connection address</h5>
+        <ClickCopy v-if="gameserver.address" :content="gameserver.address" />
+        <h5 v-if="gameserver.password && gameserver.password != 'whitelist'" class="mt-2">Password</h5>
+        <ClickCopy v-if="gameserver.password && gameserver.password != 'whitelist'" :content="gameserver.password" />
       </div>
     </div>
   </div>
--- src/views/GameserversView.vue
+++ src/views/GameserversView.vue
@@ -11,7 +11,7 @@
 
 <template>
   <div class="px-4 py-5 text-center">
-    <h1 class="display-5 fw-bold text-white" v-if="user">Dox Box Gameservers</h1>
+    <h1 class="display-6 fw-bold text-white" v-if="user">Gameservers</h1>
   </div>
   <div class="container-fluid mb-4">
     <div class="row justify-content-center row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
