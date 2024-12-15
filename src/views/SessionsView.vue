645b3bfa93e6185a982083508503683ab2898b60
Chaton <ethan@ehsawyer.uk>
2024-12-15 21:12:21 +0000

    Changed display order of sessions view

--- src/views/SessionsView.vue
+++ src/views/SessionsView.vue
@@ -7,6 +7,7 @@
     import SessionDayDropdown from '../components/SessionDayDropdown.vue';
     import UpcomingSessions from '../components/UpcomingSessions.vue';
     import SessionsCalendar from '../components/SessionsCalendar.vue';
+    import CampaignsBar from '../components/CampaignsBar.vue';
 
     const db = useDatabase();
     const user = useCurrentUser();
@@ -26,14 +27,9 @@
         return user.value.uid;
     });
 
-    const dmsCampaign = computed(() => (Object.values(campaigns.value).filter((campaign) => campaign.dm == userExtended.value.discordID).length > 0 ? Object.values(campaigns.value).find((campaign) => campaign.dm == userExtended.value.discordID) : false));
-
-    const dmsCampaignAvailable = computed(() => {
-        try {
-            return typeof dmsCampaign.value.name === 'string';
-        } catch (err) {
-            return false;
-        }
+    const dmsCampaign = computed(() => {
+        if (typeof campaigns.value !== 'undefined' && typeof userExtended.value !== 'undefined') return campaigns.value[userExtended.value.dmCampaign];
+        else return false;
     });
 </script>
 
@@ -45,25 +41,28 @@
         </p>
     </div>
     <div v-else class="container-fluid">
-        <div class="row">
+        <div class="row pb-5">
             <div class="col">
-                <div>
-                    <h4 class="fw-bold text-white pb-2">Typical Weekly Availability</h4>
-                    <WeeklyAvailability />
-                </div>
-                <div v-if="dmsCampaignAvailable" class="w-50 mx-auto pt-4">
-                    <h4 class="fw-bold text-white pb-2">{{ dmsCampaign.name }} Main Session</h4>
-                    <SessionDayDropdown :campaign="dmsCampaign" />
-                </div>
+                <h4 class="fw-bold text-white pb-4">Session Calendar</h4>
+                <SessionsCalendar />
             </div>
             <div class="col col-4 mx-5">
                 <h4 class="fw-bold text-white pb-2">Upcoming Sessions</h4>
                 <UpcomingSessions />
             </div>
             <div class="col">
-                <h4 class="fw-bold text-white pb-4">Session Calendar</h4>
-                <SessionsCalendar />
+                <div>
+                    <h4 class="fw-bold text-white pb-2">Typical Weekly Availability</h4>
+                    <WeeklyAvailability />
+                </div>
+                <div v-if="dmsCampaign" class="w-50 mx-auto pt-4">
+                    <h4 class="fw-bold text-white pb-2">{{ dmsCampaign.name }} Main Session</h4>
+                    <SessionDayDropdown :campaign="dmsCampaign" />
+                </div>
             </div>
+        </div>
+        <div class="row pt-5">
+            <CampaignsBar />
         </div>
     </div>
 </template>
